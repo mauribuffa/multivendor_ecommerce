6abcc492ad14d223ef7561005cdd4a2ed459f667
Mauricio <buffamauricio@gmail.com>
2025-01-20 09:52:55 -0300

    Add subcategory management features

---
+++ src/app/dashboard/admin/subCategories/new/page.tsx
@@ -0,0 +1,8 @@
+import SubCategoryDetails from "@/components/dashboard/forms/subCategory-details";
+import { getAllCategories } from "@/queries/category";
+
+export default async function AdminNewSubCategoryPage() {
+  const categories = await getAllCategories();
+  
+  return <SubCategoryDetails categories={categories} />;
+}
